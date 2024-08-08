e1dd4488c87b61924be2411690df05b87e4cba7b
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2024-08-08 13:02:45 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,27 +6,14 @@ const Contact = () => {
   return (
     <>
         <Resume/>
-    <div className="contactheader"></div>
-    <div className="contactsection">
-        <div className="divcover">
-        <div className="section1">
-            <div>
+        <div className="contactsection">
+        <div className="con">
         Most Honorable Guest,
-        <br></br>
-        <br></br>
+        <br /><br />
         By royal summons, we humbly request the honor of your presence at a distinguished assembly. In this noble gathering, we shall engage in conversations of great import and shared wisdom. Your esteemed presence will bestow an air of grandeur and refinement upon our meeting.
         </div>
-        </div>
-        {/* <img className='cover' src="cover.png" alt="" /> */}
-        </div>
-
-        <div className="section2">
-            {/* <div className="royal">
-                <div>A ROYAL TOAST AWAITS !</div>
-                <img src="royal.jpeg" height={100} width={100} alt="" />
-            </div> */}
+        <div className="line"><img src="./background2.png" width={300} height={300} alt="" /></div>
         <form action="https://formspree.io/f/movavggy" method="POST" >
-                <img src="letter.png" alt="" className="letter" />
                 <label className='username'>
                     Your USERNAME:
                     <input type="text" name="name"></input>
@@ -42,7 +29,7 @@ const Contact = () => {
                 <div><button type="submit">Send Forth</button></div>
                 </form>
         </div>
-    </div>
+    
       
     </>
   )
